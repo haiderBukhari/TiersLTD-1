3db071949de211d8aad38e23d68a1d36ea769ecd
Muhammad Haider Bukhari <apshaiderbukhari786@gmail.com>
2024-05-08 23:45:37 +0500

    responsive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,17 +9,17 @@ export function Header({ show, setShow }) {
                     Swiss Mining
                 </Link>
                 {
-                    !show ? <p onClick={() => { setShow(!show) }}><Menu /></p> : <p onClick={() => { setShow(!show) }}><X /></p>
+                    !show ? <p onClick={() => { setShow(true) }}><Menu /></p> : <p onClick={() => { setShow(false) }}><X /></p>
                 }
             </div>
             <Link to="/" className="mt-3.5 text-xl text-white font-medium hidden md:block" style={{ fontFamily: "Krona One" }}>
                 Swiss Mining
             </Link>
             <div className={`flex flex-col md:flex-row justify-between mt-10 md:mt-4 md:h-auto gap-5 pr-5 my-auto text-xs font-light ${show ? 'block' : 'hidden md:block'}`} style={{ fontFamily: "Krona One" }}>
-                <Link to="/miner" onClick={() => { setShow(!show) }} className="cursor-pointer mt-8 md:mt-0 mx-0 md:mx-4" style={{ zIndex: 20 }} >ASICS</Link>
-                <Link to="/miner2" onClick={() => { setShow(!show) }} className="cursor-pointer mt-8 md:mt-0 mx-0 md:mx-4" style={{ zIndex: 20 }} >Unser Angebot</Link>
-                <Link to="/miner3" onClick={() => { setShow(!show) }} className="cursor-pointer mt-8 md:mt-0 mx-0 md:mx-4" style={{ zIndex: 20 }} >Pakete</Link>
-                <Link to="/miner4" onClick={() => { setShow(!show) }} className="cursor-pointer mt-8 md:mt-0 mx-0 md:mx-4" style={{ zIndex: 20 }} >Kontakt</Link>
+                <Link to="/miner" onClick={() => { setShow(false) }} className="cursor-pointer mt-8 md:mt-0 mx-0 md:mx-4" style={{ zIndex: 20 }} >ASICS</Link>
+                <Link to="/miner2" onClick={() => { setShow(false) }} className="cursor-pointer mt-8 md:mt-0 mx-0 md:mx-4" style={{ zIndex: 20 }} >Unser Angebot</Link>
+                <Link to="/miner3" onClick={() => { setShow(false) }} className="cursor-pointer mt-8 md:mt-0 mx-0 md:mx-4" style={{ zIndex: 20 }} >Pakete</Link>
+                <Link to="/miner4" onClick={() => { setShow(false) }} className="cursor-pointer mt-8 md:mt-0 mx-0 md:mx-4" style={{ zIndex: 20 }} >Kontakt</Link>
             </div>
             <Link to="/register" style={{ zIndex: 20 }} className={`justify-center px-3 py-3.5 text-base text-center whitespace-nowrap rounded-xl border border-solid bg-[linear-gradient(225deg,#18C8FF_14.89%,#933FFE_85.85%)] border-white border-opacity-50 mt-20 md:mt-0 ${show ? 'block' : 'hidden md:block'}`}>
                 Kundenportal
