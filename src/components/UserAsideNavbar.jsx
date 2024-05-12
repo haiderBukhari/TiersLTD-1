649e17a8f7862cda36821079a58d5211d2902779
Muhammad Haider Bukhari <apshaiderbukhari786@gmail.com>
2024-05-12 05:34:16 +0500

    responsive

--- src/components/UserAsideNavbar.jsx
+++ src/components/UserAsideNavbar.jsx
@@ -7,7 +7,7 @@ const UserAsideNavbar = ({ show, setShow }) => {
             <div className={`w-full px-2 py-2  ${'block md:hidden'}`}>
                 <Menu onClick={() => setShow(!show)} className='text-white w-10 h-10 ' /> {/* Adjust the width and height as needed */}
             </div>
-            <div  className={`bg-[#242A48] bg-opacity-30 w-full md:w-[273px] h-auto pb-10 md:pb-0 md:h-[500px] relative md:block ${show ? 'block pl-3 ml-[-15px] md:ml-1 w-[240px] z-30 bg-[#242A48] h-[480px] bg-opacity-60' : 'hidden md:block'}`} style={{ borderTopRightRadius: "20px", borderBottomRightRadius: "20px", zIndex: 10, width: `${show} && 240px`, height: `${show} && 480px`}}>
+            <div  className={`bg-[#242A48] bg-opacity-30 md:w-[273px] pb-10 md:pb-0 md:h-[500px] relative md:block ${show ? 'block pl-3 ml-[-15px] md:ml-1 w-[240px] z-30 bg-[#242A48] h-[480px] bg-opacity-60' : 'hidden md:block h-auto w-full'}`} style={{ borderTopRightRadius: "20px", borderBottomRightRadius: "20px", zIndex: 10, width: `${show} && 240px`, height: `${show} && 480px`}}>
                 <div className='flex flex-col items-center pt-10 absolute md:relative' >
                     <Link style={{ fontFamily: "Inter" }} className='text-white text-2xl font-semibold opacity-100' to='/user/dashboard'>Dashboard</Link>
                     <Link style={{ fontFamily: "Inter" }} className='text-white text-2xl font-semibold mt-10' to='/user/wallet'>Pool</Link>
